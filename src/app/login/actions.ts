e1ebf91b3d7b6a14560a520d7ab8ea4707748068
mohamed youssef <51048279+moAlarand@users.noreply.github.com>
2024-11-23 14:02:46 +0200

    fixxx

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -14,13 +14,12 @@ export async function login(formData: FormData) {
     password: formData.get("password") as string,
   };
 
-  if (process.env.NODE_ENV !== "development") {
     const { error } = await supabase.auth.signInWithPassword(data);
 
     if (error) {
       redirect(`/error?message=${error.message}`);
     }
-  }
+
   revalidatePath("/", "layout");
   redirect("/stocks");
 }
